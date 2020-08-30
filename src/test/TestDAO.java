264bd9460ca485150632f175959eba87bdd71ce5
dospi <chibisov33@gmail.com>
2020-08-30 09:48:28 +0300

    hibernate config fix

--- src/test/TestDAO.java
+++ src/test/TestDAO.java
@@ -9,15 +9,16 @@ public class TestDAO {
 	private Session session;
 	private Transaction tx;
 	public TestDAO(){
-		//session = HibernateSessionFactoryUtil.getSessionFactory().getCurrentSession();
+		session = HibernateSessionFactoryUtil.getSessionFactory().getCurrentSession();
 	}
 	@Test
 	public void testAccount()
 	{
-		//tx = session.beginTransaction();
-		//AccountDao acc = new AccountDao();
-		//int size = acc.getAll().size();
-		//Assert.assertEquals(size, 3);
-		//tx.commit();
+		tx = session.beginTransaction();
+		System.out.print(session);
+		AccountDao acc = new AccountDao();
+		int size = acc.getAll().size();
+		Assert.assertEquals(size, 3);
+		tx.commit();
 	}
 } 
