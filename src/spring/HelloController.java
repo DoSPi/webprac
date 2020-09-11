da828e3616567d905d00b276ae342496a29d0b5c
dospi <chibisov33@gmail.com>
2020-09-11 15:22:21 +0300

    spring java config

--- src/spring/HelloController.java
+++ src/spring/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
 
 @Controller
-@RequestMapping("/hello")
+@RequestMapping("/")
 public class HelloController {
    @RequestMapping(method = RequestMethod.GET)public String printHello(ModelMap model) {
       model.addAttribute("message", "Hello Spring MVC Framework!");
---
+++ src/spring/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+ package spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"spring"})
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
